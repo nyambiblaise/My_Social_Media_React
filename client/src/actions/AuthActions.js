a18f831babd190961c0a26648b97016ea29c68d3
Nyambi Blaise <prof.masterb.ise@gmail.com>
2023-07-15 18:38:06 -0400

    f

--- client/src/actions/AuthActions.js
+++ client/src/actions/AuthActions.js
@@ -10,7 +10,6 @@ export const logIn = (formData, navigate) => async (dispatch) => {
     dispatch({ type: "AUTH_FAIL" });
   }
 };
-
 export const signUp = (formData, navigate) => async (dispatch) => {
   dispatch({ type: "AUTH_START" });
   try {
